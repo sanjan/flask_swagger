f4e63157d6987c3f6c3fbb0703f52de0cef7c727
Sanjan Grero <sgrero@sgrero-ltm2.internal.salesforce.com>
2020-07-15 13:48:39 +0930

    initial commit

---
+++ hello_api.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, request, jsonify, render_template
+
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def get_root():
+    print('sending root')
+    return render_template('index.html')
+
+@app.route('/api/docs')
+def get_docs():
+    print('sending docs')
+    return render_template('swaggerui.html')
+
+
+@app.route('/api')
+def get_api():
+    hello_dict = {'en': 'Hello', 'es': 'Hola'}
+    lang = request.args.get('lang')
+    return jsonify(hello_dict[lang])
+
+
+app.run(use_reloader=True, debug=False)
